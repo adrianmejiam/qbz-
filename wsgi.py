474ac4103f7a939feb9a6105cb5cd3e161f3792a
Adrián Mejía <51228667+adrianmejiam@users.noreply.github.com>
2022-05-15 05:18:27 -0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,2 +1,3 @@
+from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application),
+application = DjangoWhiteNoise(get_wsgi_application())
